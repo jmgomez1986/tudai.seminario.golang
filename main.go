b8766a75954d35de7694e5d188a28aeb51e9e5a2
jmgomez1986 <jmgametal@gmail.com>
2020-10-31 16:40:41 -0300

    Tipo Función 1

--- main.go
+++ main.go
@@ -2,37 +2,17 @@ package main
 
 import "fmt"
 
-type person struct {
-	name string
-}
+type close func(s string)
 
-type figure struct {
-	name string
-}
-
-// Printable ...
-type Printable interface {
-	print()
-}
-
-func (p *person) print() {
-	fmt.Println("[person]", p.name)
-}
-
-func (f *figure) print() {
-	fmt.Println("[figure]", f.name)
-}
-
-func invokePint(p Printable) {
-	p.print()
+func invokeClose(c close) {
+	c("Hello world")
 }
 
 func main() {
-	p := &person{name: "Juan"}
-	f := &figure{"Circulo"}
-	// Se pueden pasar los atributos por orden o con el nombre de atributo, se recomienda indicando el nombre
+	f := func(s string) {
+		fmt.Println(s)
+	}
 
-	invokePint(p)
-	invokePint(f)
+	invokeClose(f)
 
 }
