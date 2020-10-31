de5424a8d67a0f2392dcd9a3bc1d752da238bc79
jmgomez1986 <jmgametal@gmail.com>
2020-10-31 15:44:09 -0300

    Estructuras de datos: Slice

--- main.go
+++ main.go
@@ -5,11 +5,30 @@ import (
 )
 
 func main() {
-	var arr [2]int
-	arr[0] = 1
-	arr[1] = 2
+	/*	Un slice tiene tamaño dinamico son punteros a estructuras dinamucas.
+		 Es una estructura con 3 valores:
+			1- Un arrego comun de tamaño fijo
+			2- Capacity
+			3- Lenght
+	*/
 
-	for index, value := range arr { // El 'range' devuelve cada una de las posiciones del arreglo
+	// var l []int
+	l := make([]int, 100)
+	// El := define a 'l' con el tipo de lo que devuelve el miembro de la izquierda
+	// El make devuelve un slice con la capacidad indicada, al superar la capacidad, vuelve a generar un sline nuevo
+	l = append(l, 10)
+	// El 'apprend esta creando un nuevo slice con todos los elementos de l agregando el valor
+	fmt.Printf("%p\n", l) // Aca mostramos la direccion de memoria
+
+	l = append(l, 100)
+	fmt.Printf("%p\n", l)
+
+	l = append(l, 1000)
+	fmt.Printf("%p\n", l)
+
+	fmt.Println(l)
+
+	for index, value := range l {
 		fmt.Println(index, value)
 	}
 }
