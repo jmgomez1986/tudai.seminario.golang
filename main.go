a870cd25fefb354bc1ae93eeac18796c6725a142
jmgomez1986 <jmgametal@gmail.com>
2020-10-31 15:51:55 -0300

    Estructuras de datos: Map

--- main.go
+++ main.go
@@ -1,34 +1,13 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	/*	Un slice tiene tamaño dinamico son punteros a estructuras dinamucas.
-		 Es una estructura con 3 valores:
-			1- Un arrego comun de tamaño fijo
-			2- Capacity
-			3- Lenght
-	*/
+	m := make(map[int]string)
+	m[0] = "a"
+	m[1] = "b"
 
-	// var l []int
-	l := make([]int, 100)
-	// El := define a 'l' con el tipo de lo que devuelve el miembro de la izquierda
-	// El make devuelve un slice con la capacidad indicada, al superar la capacidad, vuelve a generar un sline nuevo
-	l = append(l, 10)
-	// El 'apprend esta creando un nuevo slice con todos los elementos de l agregando el valor
-	fmt.Printf("%p\n", l) // Aca mostramos la direccion de memoria
-
-	l = append(l, 100)
-	fmt.Printf("%p\n", l)
-
-	l = append(l, 1000)
-	fmt.Printf("%p\n", l)
-
-	fmt.Println(l)
-
-	for index, value := range l {
-		fmt.Println(index, value)
+	for key, val := range m {
+		fmt.Println(key, val)
 	}
 }
