f9d69d49699a52bf2634ae5ead2becbaefb0915e
jmgomez1986 <jmgametal@gmail.com>
2020-10-31 16:12:18 -0300

    Tipos

--- main.go
+++ main.go
@@ -2,12 +2,27 @@ package main
 
 import "fmt"
 
-func main() {
-	m := make(map[int]string)
-	m[0] = "a"
-	m[1] = "b"
+type person struct {
+	name string
+}
 
-	for key, val := range m {
-		fmt.Println(key, val)
-	}
+func (p *person) print() {
+	fmt.Println(p.name)
+}
+
+func (p *person) clean() {
+	p.name = ""
+}
+
+// Poniendo el '*' usa un puntero a la misma ponsicion de memoria del que lo llamo, si no hace una copia
+
+func main() {
+	p := &person{name: "Juan"}
+	// Crea una instancia de la estructura 'Person',
+	//le agrega valor al atributo y obtiene el valor del puntero y lo guarda en 'p'
+
+	p.print()
+	p.clean()
+	p.print()
+
 }
