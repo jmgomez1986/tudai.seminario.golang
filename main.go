b1959810041c1b6c097178ba556745920d524a69
jmgomez1986 <jmgametal@gmail.com>
2020-10-31 15:14:02 -0300

    Estructuras de datos: Array

--- main.go
+++ main.go
@@ -1,23 +1,15 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 )
 
-func suma(a, b int) (int, error) {
-	if a < b {
-		return 0, errors.New("el primer valor es menor que el segundo")
-	}
-	return a + b, nil
-}
-
 func main() {
-	// fmt.Println("Hello world!!!");
-	r, err := suma(1, 2)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(r)
+	var arr [2]int
+	arr[0] = 1
+	arr[1] = 2
+
+	for index, value := range arr { // El 'range' devuelve cada una de las posiciones del arreglo
+		fmt.Println(index, value)
 	}
 }
