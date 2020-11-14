2f693e740eba4be90136c0ebb3c5d6d4203bf8b9
jmgomez2018 <jmgametal@gmail.com>
2020-11-14 14:39:22 -0300

    Ejemplo de API REST (Get y Post)

--- main.go
+++ main.go
@@ -1,22 +1,28 @@
 package main
 
-import "fmt"
-
-type close func(s string)
-
-func invokeClose(c close) {
-	c("Hello world")
-}
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	// f := func(s string) {
-	// 	fmt.Println(s)
-	// }
+	r := gin.Default()
+	r.GET("/users/:name", getUserHandler)
+	r.POST("/users", addUserHandler)
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
 
-	// invokeClose(f)
-
-	invokeClose(func(s string) {
-		fmt.Println(s)
+func getUserHandler(c *gin.Context) {
+	name := c.Param("name")
+	lastname := c.Query("lastname")
+	c.JSON(200, gin.H{
+		"status": "ok",
+		"name":   name + " " + lastname,
 	})
+}
 
+func addUserHandler(c *gin.Context) {
+	c.JSON(201, gin.H{
+		"name":     "matias",
+		"lastname": "gomez",
+	})
 }
